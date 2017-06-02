38b0d087d310d0839f7f2d1924d7d9db47eb7c4b
Scott <marden.scott@gmail.com>
2017-06-02 13:11:08 -0700

    Commiting submitted version of assignment. Going to attempt to add randomized button if there is extra time.

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 $(document).ready(function(){
 
+	// function randomPoke(){
+	// 	var randomId = Math.floor(Math.random())
+	// 	console.log(res);
+	// 	$("#info").html("<img src='http://pokeapi.co/media/img/"+id+".png' /><h3>"+res.name+"</h3>");
+	// 	$("#info").append("<h4>Types</h4><ul></ul>");
+	// 	for (var j = 0; j < res.types.length; j++){
+	// 		$("#info ul").append("<li>"+res.types[j].name+"</li>");
+	// 	};
+	// 	$("#info").append("<h4>Height</h4><p>"+res.height+"</p>");
+	// 	$("#info").append("<h4>Weight</h4><p>"+res.weight+"</p>");
+	// }
+
 	var htmlStr = "";
 
 	for(var i = 1; i <= 151; i++){
 
-		htmlStr = "<img src='http://pokeapi.co/media/img/"+i+".png' alt='pokemon image'/>";
+		htmlStr = "<img id='"+i+"' src='http://pokeapi.co/media/img/"+i+".png' alt='pokemon image'/>";
 		$("#images").append(htmlStr);
 	};
+
+	$(document).on("click", "img", function(){
+		var id = $(this).attr("id");
+		var apiCall = "http://pokeapi.co/api/v1/pokemon/"+id+"/";
+		// console.log(id);
+		$.get(apiCall, function(res){
+			console.log(res);
+			$("#info").html("<img src='http://pokeapi.co/media/img/"+id+".png' /><h3>"+res.name+"</h3>");
+			$("#info").append("<h4>Types</h4><ul></ul>");
+			for (var j = 0; j < res.types.length; j++){
+				$("#info ul").append("<li>"+res.types[j].name+"</li>");
+			};
+			$("#info").append("<h4>Height</h4><p>"+res.height+"</p>");
+			$("#info").append("<h4>Weight</h4><p>"+res.weight+"</p>");
+		}, 'json');
+
+	$("button").click(randomPoke());
+
+	});
 
 });
